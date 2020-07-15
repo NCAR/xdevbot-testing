e67b4a1539ce44d65c55e9cec1bc49b7ab74babe
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-07-15 12:49:14 -0600

    Push changes to master

--- .github/workflows/register_repo.py
+++ .github/workflows/register_repo.py
@@ -4,8 +4,6 @@ import os
 import sys
 import json
 
-# import subprocess
-
 if __name__ == "__main__":
     with open(os.environ["GITHUB_EVENT_PATH"], "r") as f:
         event_payload = json.load(f)
@@ -31,11 +29,6 @@ if __name__ == "__main__":
             config[info["campaign"]]["repos"].append(info["repo"])
             with open(config_file, "w") as file_obj:
                 yaml.dump(config, file_obj, indent=4)
-                git_config_id = 'git config --global user.email "action@github.com" && git config --global user.name "GitHub Action"'
-                git_commit = 'git add config.yaml && git commit -m "[skip ci] Update configuration file."'
-                git_push = f"git push -f --set-upstream origin master"
-                command = f"""{git_config_id} && {git_commit} && {git_push}"""
-                # subprocess.check_call(command, shell=True, stdout=subprocess.PIPE)
 
         else:
             print(f"{info['repo']} is registered already.")
